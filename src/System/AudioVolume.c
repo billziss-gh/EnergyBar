9c70758130f268f3d8f9bcf8df3b8ad178a9d2cc
Bill Zissimopoulos <billziss@navimatics.com>
2018-08-18 19:52:16 -0700

    audio volume

--- src/System/AudioVolume.c
+++ src/System/AudioVolume.c
@@ -12,13 +12,70 @@
  */
 
 #include "AudioVolume.h"
+#include <CoreAudio/CoreAudio.h>
+#include <AudioToolbox/AudioServices.h>
+#include <pthread.h>
+
+static pthread_once_t audio_dev_once = PTHREAD_ONCE_INIT;
+static AudioDeviceID audio_dev = kAudioObjectUnknown;
+
+static void audio_dev_initonce(void)
+{
+    AudioObjectPropertyAddress address =
+    {
+        .mSelector = kAudioHardwarePropertyDefaultOutputDevice,
+        .mScope = kAudioObjectPropertyScopeGlobal,
+        .mElement = kAudioObjectPropertyElementMaster,
+    };
+    AudioDeviceID device = kAudioObjectUnknown;
+    UInt32 size = sizeof device;
+
+    if (kAudioHardwareNoError !=
+        AudioObjectGetPropertyData(kAudioObjectSystemObject, &address, 0, 0, &size, &device))
+        return;
+
+    audio_dev = device;
+}
 
 double GetAudioVolume(void)
 {
-    return 0.5;
+    pthread_once(&audio_dev_once, audio_dev_initonce);
+    if (kAudioObjectUnknown == audio_dev)
+        return NAN;
+
+    AudioObjectPropertyAddress address =
+    {
+        .mSelector = kAudioHardwareServiceDeviceProperty_VirtualMasterVolume,
+        .mScope = kAudioDevicePropertyScopeOutput,
+        .mElement = kAudioObjectPropertyElementMaster,
+    };
+    Float32 volume = NAN;
+    UInt32 size = sizeof volume;
+
+    if (kAudioHardwareNoError !=
+        AudioObjectGetPropertyData(audio_dev, &address, 0, 0, &size, &volume))
+        return NAN;
+
+    return volume;
 }
 
-bool SetAudioVolume(double volume)
+bool SetAudioVolume(double volume0)
 {
+    pthread_once(&audio_dev_once, audio_dev_initonce);
+    if (kAudioObjectUnknown == audio_dev)
+        return false;
+
+    AudioObjectPropertyAddress address =
+    {
+        .mSelector = kAudioHardwareServiceDeviceProperty_VirtualMasterVolume,
+        .mScope = kAudioDevicePropertyScopeOutput,
+        .mElement = kAudioObjectPropertyElementMaster,
+    };
+    Float32 volume = volume0;
+
+    if (kAudioHardwareNoError !=
+        AudioObjectSetPropertyData(audio_dev, &address, 0, 0, sizeof volume, &volume))
+        return false;
+
     return true;
 }
