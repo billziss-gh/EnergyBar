6c91a9ca22019609d0e627e92920936214921fc2
Bill Zissimopoulos <billziss@navimatics.com>
2018-09-01 16:22:25 -0700

    FolderController: quick look icons

--- src/FolderController.m
+++ src/FolderController.m
@@ -12,20 +12,22 @@
  */
 
 #import "FolderController.h"
+#import <QuickLook/QuickLook.h>
 #import "ImageTitleView.h"
 
-static NSSize smallItemSize = { 50, 30 };
-static NSSize largeItemSize = { 150, 30 };
+static const NSSize smallItemSize = { 50, 30 };
+static const NSSize largeItemSize = { 150, 30 };
+static const NSUInteger maxFileCount = 100;
 
 @interface FolderItem : NSObject
-@property (retain) NSString *name;
+@property (retain) NSURL *url;
 @property (retain) NSImage *icon;
 @end
 
 @implementation FolderItem
 - (void)dealloc
 {
-    self.name = nil;
+    self.url = nil;
     self.icon = nil;
     [super dealloc];
 }
@@ -66,7 +68,7 @@ static NSSize largeItemSize = { 150, 30 };
 @property (retain) IBOutlet NSTextField *label;
 @property (retain) IBOutlet NSButton *emptyButton;
 @property (retain) IBOutlet NSButton *openButton;
-@property (retain) NSArray *contents;
+@property (retain) NSArray<FolderItem *> *contents;
 @property (assign) BOOL tooManyItems;
 @end
 
@@ -98,25 +100,26 @@ static NSSize largeItemSize = { 150, 30 };
     NSDirectoryEnumerator *enumerator = [[NSFileManager defaultManager]
         enumeratorAtURL:self.url
         includingPropertiesForKeys: nil != self.sortKey ?
-            [NSArray arrayWithObject:self.sortKey] : nil
+            [NSArray arrayWithObjects:NSURLIsDirectoryKey, NSURLIsApplicationKey, self.sortKey, nil] :
+            nil
         options:
             (self.includeDescendants ? 0 : NSDirectoryEnumerationSkipsSubdirectoryDescendants) |
             NSDirectoryEnumerationSkipsPackageDescendants |
             NSDirectoryEnumerationSkipsHiddenFiles
         errorHandler:nil];
-    NSMutableArray *contents = [NSMutableArray array];
+    NSMutableArray<NSURL *> *urls = [NSMutableArray array];
     BOOL tooManyItems = NO;
     NSURL *url;
     while (0 != (url = [enumerator nextObject]))
     {
-        if (100 <= contents.count)
+        if (maxFileCount <= urls.count)
         {
             tooManyItems = YES;
             break;
         }
-        [contents addObject:url];
+        [urls addObject:url];
     }
-    [contents sortUsingComparator:^NSComparisonResult(id obj1, id obj2)
+    [urls sortUsingComparator:^NSComparisonResult(id obj1, id obj2)
     {
         NSURL *url1 = obj1;
         NSURL *url2 = obj2;
@@ -132,6 +135,23 @@ static NSSize largeItemSize = { 150, 30 };
         }
         return [url1.path localizedStandardCompare:url2.path];
     }];
+
+    NSMutableArray<FolderItem *> *contents = [NSMutableArray array];
+    NSImage *appIcon = [[NSWorkspace sharedWorkspace] iconForFileType:@".app"];
+    NSImage *dirIcon = [[NSWorkspace sharedWorkspace] iconForFileType:@"public.folder"];
+    NSImage *docIcon = [[NSWorkspace sharedWorkspace] iconForFileType:@"public.content"];
+    for (NSURL *url in urls)
+    {
+        NSNumber *value;
+        BOOL isDir = [url getResourceValue:&value forKey:NSURLIsDirectoryKey error:0] && [value boolValue];
+        BOOL isApp = [url getResourceValue:&value forKey:NSURLIsApplicationKey error:0] && [value boolValue];
+
+        FolderItem *item = [[FolderItem alloc] init];
+        item.url = url;
+        item.icon = isApp ? appIcon : (isDir ? dirIcon : docIcon);
+        [contents addObject:item];
+        [item release];
+    }
     self.contents = contents;
     self.tooManyItems = tooManyItems;
 
@@ -156,6 +176,10 @@ static NSSize largeItemSize = { 150, 30 };
     }
     self.touchBar.defaultItemIdentifiers = itemIdentifiers;
 
+    [self
+        performSelectorInBackground:@selector(prepareIconsInBackground:)
+        withObject:[[urls copy] autorelease]/* be paranoid */];
+
     return [super presentWithPlacement:placement];
 }
 
@@ -163,8 +187,75 @@ static NSSize largeItemSize = { 150, 30 };
 {
     self.contents = nil;
     self.url = nil;
+    [self.scrubber reloadData];
 
     [super dismiss];
+}
+
+- (void)prepareIconsInBackground:(NSArray<NSURL *> *)urls
+{
+    @autoreleasepool
+    {
+        NSMutableDictionary *icons = [NSMutableDictionary dictionary];
+        NSSize size = NSMakeSize(60, 60);
+        NSDictionary *options = [NSDictionary dictionaryWithObjectsAndKeys:
+            [NSNumber numberWithBool:YES], kQLThumbnailOptionIconModeKey,
+            nil];
+        for (NSURL *url in urls)
+        {
+            NSImage *icon = nil;
+            CGImageRef cgimage = QLThumbnailImageCreate(
+                0, (CFURLRef)url, size, (CFDictionaryRef)options);
+            if (0 != cgimage)
+            {
+                icon = [[[NSImage alloc] initWithCGImage:cgimage size:NSZeroSize] autorelease];
+                CGImageRelease(cgimage);
+            }
+
+            if (nil == icon)
+                icon = [[NSWorkspace sharedWorkspace] iconForFile:url.path];
+
+            if (nil != icon)
+                [icons setObject:icon forKey:url];
+
+            if (0 < icons.count && 0 == icons.count % 4)
+            {
+                [self
+                    performSelectorOnMainThread:@selector(updateIcons:)
+                    withObject:[[icons copy] autorelease]
+                    waitUntilDone:NO];
+                [icons removeAllObjects];
+            }
+        }
+
+        if (0 < icons.count)
+            [self
+                performSelectorOnMainThread:@selector(updateIcons:)
+                withObject:[[icons copy] autorelease]
+                waitUntilDone:NO];
+    }
+}
+
+- (void)updateIcons:(NSDictionary *)icons
+{
+    [self.scrubber performSequentialBatchUpdates:^
+    {
+        NSArray<FolderItem *> *contents = self.contents;
+        for (NSURL *url in icons)
+        {
+            NSUInteger index = 0;
+            for (FolderItem *item in contents)
+            {
+                if ([item.url isEqual:url])
+                {
+                    item.icon = [icons objectForKey:url];
+                    [self.scrubber reloadItemsAtIndexes:[NSIndexSet indexSetWithIndex:index]];
+                    break;
+                }
+                index++;
+            }
+        }
+    }];
 }
 
 - (IBAction)emptyButtonAction:(id)sender
@@ -186,10 +277,11 @@ static NSSize largeItemSize = { 150, 30 };
 
 - (NSScrubberItemView *)scrubber:(NSScrubber *)scrubber viewForItemAtIndex:(NSInteger)index
 {
-    NSURL *url = [self.contents objectAtIndex:index];
+    FolderItem *item = [self.contents objectAtIndex:index];
     FolderItemView *view = [self.scrubber makeItemWithIdentifier:@"item" owner:nil];
-    view.imageTitleView.image = [[NSWorkspace sharedWorkspace] iconForFile:url.path];
-    view.imageTitleView.title = NSImageOnly != self.imagePosition ? [url.path lastPathComponent] : @"";
+    view.imageTitleView.image = item.icon;
+    view.imageTitleView.title = NSImageOnly != self.imagePosition ?
+        [item.url lastPathComponent] : @"";
     [view setFrameSize:NSImageOnly != self.imagePosition ? largeItemSize : smallItemSize];
     return view;
 }
@@ -198,19 +290,8 @@ static NSSize largeItemSize = { 150, 30 };
 {
     if ([self.delegate respondsToSelector:@selector(folderController:didSelectURL:)])
     {
-        NSURL *url = [self.contents objectAtIndex:index];
-        [self.delegate folderController:self didSelectURL:url];
+        FolderItem *item = [self.contents objectAtIndex:index];
+        [self.delegate folderController:self didSelectURL:item.url];
     }
 }
 @end
-
-#if 0
-static NSSize iconSize = { 30, 30 };
-static CGFloat spacerWidth = 4;
-
-@implementation DockWidgetFolderController
-
-
-@end
-#endif
-
