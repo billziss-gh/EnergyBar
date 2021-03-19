90893f40bdf6356c9fa402b4fdb513096d94f019
knives-repo <kevsperanza@gmail.com>
2021-03-19 17:42:03 +0000

    Added delay to infoDidChange execution of updateInfo in order to fix album art for Music and Spotify.
    NB: Main issue is that album art doesn't *immediately* pop up when changing/unpausing music. Adding a 250ms delay allows the album art data to come up before the update executes.

--- src/System/NowPlaying.m
+++ src/System/NowPlaying.m
@@ -215,7 +215,14 @@ extern NSString *kMRMediaRemoteNowPlayingInfoArtworkData;
 
 - (void)infoDidChange:(NSNotification *)notification
 {
-    [self updateInfo];
+    /*
+     The 250ms delay is necessary for album art to update correctly.
+     Potentially, one can experiment with placing another [self updateInfo] before the delay to repeat this action, but this results in some wonky icon changes.
+     */
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(250 * NSEC_PER_MSEC)), dispatch_get_main_queue(),
+       ^{
+        [self updateInfo];
+        });
 }
 
 - (void)playingDidChange:(NSNotification *)notification
